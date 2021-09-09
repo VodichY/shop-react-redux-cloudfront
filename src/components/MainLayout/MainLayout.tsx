779243fbc831b40d0067994a9334ad1564ca0604
VodzichY <yuri.vodich@gmail.com>
2021-09-09 03:09:52 +0300

    integration with be

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © vodzichY '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        sneakers store
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
